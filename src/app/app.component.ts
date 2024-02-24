b06e56992803a1c50f82253780f991439220552a
Helelys <tallyslabanca@outlook.com>
2024-02-23 21:47:52 -0300

    components styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component'
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component'
+import { HomeComponent } from './pages/home/home.component'
+import { BigCardComponent } from './components/big-card/big-card.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenuTitleComponent,
+    MenuBarComponent,
+    HomeComponent,
+    BigCardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
